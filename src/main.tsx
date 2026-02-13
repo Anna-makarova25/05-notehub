d7eb61a6b8a86b272ea3757bf45c74684fb3443c
Anna Makarova <makarovaanna07@gmail.com>
2026-02-13 21:19:02 +0200

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+// src/main.tsx
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+const queryClient = new QueryClient();
+
+import { createRoot } from 'react-dom/client';
+import App from './components/App/App';
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+    <ReactQueryDevtools initialIsOpen={false} />
+  </QueryClientProvider>,
+);
